5471410939ffb1f1fecd2391b8535f2406417977
EraAmate <valeriiaera@gmail.com>
2020-03-03 14:18:17 +0100

    Add input functionality. Text is shown below

--- src/app.js
+++ src/app.js
@@ -9,9 +9,28 @@ export function app() {
   const main = createElement('main', {});
   const titleElement = title('Digimons Cards');
   const searchElement = search();
+  const inputResult = createElement('div', {
+    className: 'inputResult'
+  });
+  const inpuResultText = createElement('p', {
+    innerText: '',
+    className: 'inputResultText'
+  });
+
+  searchElement.addEventListener('input', event => {
+    inpuResultText.innerText = searchElement.value;
+    return inpuResultText.innerText;
+  });
+
+  searchElement.addEventListener('input', event => {
+    console.log(event.target.value, searchElement.value);
+  });
 
   header.appendChild(titleElement);
   main.appendChild(searchElement);
+
+  main.appendChild(inputResult);
+  inputResult.appendChild(inpuResultText);
 
   return [header, main];
 }
