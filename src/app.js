2279b284ce2a5637d75a741a6eeb5279c89e0ca0
EraAmate <valeriiaera@gmail.com>
2020-03-04 15:25:03 +0100

    rewrote app.js structure, rename digi class

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import './app.scss';
 import { createElement } from './lib/dom';
 import { title } from './components/title';
 import { subtitle } from './components/title';
-
+import { appendContent } from './lib/dom';
 import { search } from './components/search';
-import { digimons } from './components/digimons';
+import { digimonsAll } from './components/digimons';
 import DigiLogo from './assets/Digimon_logo.png';
 
 const allDigimons = [
@@ -15,59 +15,57 @@ const allDigimons = [
   'Antylamon',
   'Arkadimon'
 ];
+
+function filterDigimons(searchValue) {
+  const lowerCaseSearchValue = searchValue.toLowerCase();
+
+  const filteredDigimons = allDigimons.filter(digimon => {
+    return digimon.toLowerCase().startsWith(lowerCaseSearchValue);
+  });
+  return filteredDigimons;
+}
+
 export function app() {
   const header = createElement('header', {
     className: 'header'
   });
-
   const main = createElement('main', {
     className: 'main'
   });
   const titleElement = title('Digitorium');
   const subtitleElement = subtitle('Finde dein digitales monster');
-  const searchElement = search();
+  const searchInitValue = sessionStorage.getItem('searchValue'); //
+  const searchElement = search(searchInitValue); // oder search(sessionStorage.getItem("searchValue"))
 
   const DLogo = createElement('img', {
     className: 'logo',
     src: DigiLogo
   });
 
-  header.appendChild(DLogo);
-  header.appendChild(titleElement);
-  main.appendChild(subtitleElement);
-  main.appendChild(searchElement);
+  let digimons = null;
 
-  const searchResults = createElement('div', {});
+  function setSearchResults() {
+    const filteredDigimons = filterDigimons(searchElement.value);
+    digimons = digimonsAll(filteredDigimons);
+    appendContent(main, digimons);
+  }
+  setSearchResults();
+  // header.appendChild(DLogo);
+  // header.appendChild(titleElement);
+  // main.appendChild(subtitleElement);
+  // main.appendChild(searchElement);
+  // main.appendChild(digimons);
 
-  main.appendChild(searchResults);
+  appendContent(header, [DLogo, titleElement]);
+  appendContent(main, [subtitleElement, searchElement, digimons]);
 
   searchElement.addEventListener('input', event => {
-    searchResults.innerHTML = ''; // clear results
+    main.removeChild(digimons);
+    setSearchResults();
 
     const searchValue = event.target.value;
-    const filteredDigimons = allDigimons.filter(digimon => {
-      return digimon.startsWith(searchValue);
-    });
-
-    let digimonsElement = digimons(filteredDigimons);
-    searchResults.appendChild(digimonsElement);
+    sessionStorage.setItem('searchValue', searchValue);
   });
 
   return [header, main];
 }
-
-// let pokemons = createPokemons(allPokemons);
-
-// appendContent(header, [logo, title]);
-// appendContent(main, [searchInput, pokemons]);
-
-// searchInput.addEventListener('input', event => {
-//   main.removeChild(pokemons);
-
-//   const searchValue = event.target.value;
-//   const filteredPokemons = allPokemons.filter(pokemon => {
-//     return pokemon.startsWith(searchValue);
-//   });
-//   pokemons = createPokemons(filteredPokemons);
-//   appendContent(main, pokemons);
-// });
